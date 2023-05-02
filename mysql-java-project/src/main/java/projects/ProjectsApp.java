305dc9622c4f343584c0af2a42c31e441f9cf128
amanda81930 <126511804+amanda81930@users.noreply.github.com>
2023-05-02 16:49:38 -0700

    wk8 mysql-java-project

---
+++ mysql-java-project/src/main/java/projects/ProjectsApp.java
@@ -0,0 +1,14 @@
+package projects;
+
+import projects.dao.DbConnection;
+
+public class ProjectsApp {
+
+  public static void main(String[] args) {
+
+    DbConnection.getConnection(); 
+    
+
+  } //end of Main METHOD
+
+} //end of projects CLASS
